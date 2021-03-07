d1b2f86947d80bce7844280769e0a482265f231d
Mathieu McMurray <mathieu.mcmurray@Norimaconsulting.com>
2021-03-07 14:18:03 -0600

    Fix style injection issues w/ MUI & Next

--- components/fretboard/Fretboard.tsx
+++ components/fretboard/Fretboard.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import String from "./String";
+import styles from "@/styles/Fretboard.module.scss";
+import { Box } from "@material-ui/core";
 
 interface Props {}
 
@@ -7,14 +9,14 @@ function Fretboard(props: Props) {
   const {} = props;
 
   return (
-    <>
+    <Box className={styles.fretboard}>
       <String baseNote="E" />
       <String baseNote="B" />
       <String baseNote="G" />
       <String baseNote="D" />
       <String baseNote="A" />
       <String baseNote="E" />
-    </>
+    </Box>
   );
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,14 @@ interface Props {
 }
 
 function MyApp({ Component, pageProps }: Props) {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <>
       <ThemeProvider theme={lightTheme}>
