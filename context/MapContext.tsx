268937f2461bfa8380949db0b3da5f2f593f7d42
Mathieu McMurray <mathieu.mcmurray@Norimaconsulting.com>
2021-03-07 13:56:05 -0600

    Create application context

---
+++ context/MapContext.tsx
@@ -0,0 +1,86 @@
+import { Notes } from "@/model/Note";
+import React, { createContext, useContext, useReducer } from "react";
+
+enum ActionType {}
+
+export type Payload = {};
+
+export type Action = {
+  type: ActionType;
+  payload?: Payload;
+};
+
+interface Tuning {
+  string1: Notes;
+  string2: Notes;
+  string3: Notes;
+  string4: Notes;
+  string5: Notes;
+  string6: Notes;
+}
+
+interface State {
+  tuning: Tuning;
+}
+
+const initialState: State = {
+  tuning: {
+    string1: Notes.E,
+    string2: Notes.B,
+    string3: Notes.G,
+    string4: Notes.D,
+    string5: Notes.A,
+    string6: Notes.E,
+  },
+};
+
+type Dispatch = (action: Action) => void;
+const MapStateContext = createContext<State | undefined>(initialState);
+const MapDispatchContext = createContext<Dispatch | undefined>(undefined);
+
+function appReducer(state: State, action: Action): State {
+  const { type, payload } = action;
+
+  switch (type) {
+    default:
+      return state;
+  }
+}
+
+type ProviderProps = { children: React.ReactNode };
+
+function MapProvider({ children }: ProviderProps) {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+
+  return (
+    <MapStateContext.Provider value={state}>
+      <MapDispatchContext.Provider value={dispatch}>
+        {children}
+      </MapDispatchContext.Provider>
+    </MapStateContext.Provider>
+  );
+}
+
+function useMapState() {
+  const context = useContext(MapStateContext);
+  if (context === undefined) {
+    throw new Error("useMapState must be used within a MapProvider");
+  }
+
+  return context;
+}
+
+function useMapDispatch() {
+  const context = useContext(MapDispatchContext);
+  if (context === undefined) {
+    throw new Error("useMapDispatch must be used within a MapProvider");
+  }
+  return context;
+}
+
+export {
+  ActionType,
+  MapProvider,
+  useMapState,
+  useMapDispatch,
+};
